faef1f129d7d0e385d341acfc4eff468908ee4da
Null233 <1170656060@qq.com>
2019-05-12 19:11:34 +0800

    scripts-toserver

--- scripts/SendToServer.py
+++ scripts/SendToServer.py
@@ -1,3 +1,20 @@
 import requests
 import json
 
+key = '123-45-678'
+ip = 'http://localhost'
+port = 39000
+
+
+with open('./DataTemplate.json') as f:
+    load_dict = json.load(f)
+
+while 1:
+    for i in range(len(load_dict['sensors'])):
+        load_dict['sensors'][i]['type']['currentvalue'] = db
+    for i in range(len(load_dict['accessories'])):
+        for j in range(len(load_dict['accessories'][i]['iids'])):
+            url = ip + ':' + str(port) + '/characteristics?id=' + str(load_dict['accessories'][i]['aid']) + '.' + str(load_dict['accessories'][i]['iids'][j]['iid'])
+            r = requests.get(url)
+            di = json.loads(r.text)
+            load_dict['accessories'][i]['iids'][j]['currentvalue'] = di['characteristics'][0]['value']
